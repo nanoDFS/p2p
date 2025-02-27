78ba88ccc486900dcdf3c2ea49a4a1b1e4378372
nagarajRPoojari <np137270@gmail.com>
2025-02-27 22:55:59 +0530

    Release v0.0.6

--- release.sh
+++ release.sh
@@ -9,17 +9,16 @@ echo "Latest tag: $LATEST_TAG"
 
 VERSION_PARTS=($(echo $LATEST_TAG | sed 's/v//g' | tr '.' ' '))
 
-PATCH_VERSION=${VERSION_PARTS[2]}
+PATCH_VERSION=${VERSION_PARTS[3]}
 NEW_PATCH_VERSION=$((PATCH_VERSION + 1))
 
-NEW_VERSION="v${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.$NEW_PATCH_VERSION"
+NEW_VERSION="v${VERSION_PARTS[1]}.${VERSION_PARTS[2]}.$NEW_PATCH_VERSION"
 echo "New version: $NEW_VERSION"
 
 git add .
 git commit -m "Release $NEW_VERSION"
 
 git tag $NEW_VERSION
-
 
 git push origin main
 git push origin $NEW_VERSION
